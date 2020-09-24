bc0470a91922cfe8e18aa92abfa4268c9bd33a7f
hiroki_iizuka <alljapan.ma.kabu@gmail.com>
2020-09-24 10:58:39 +0900

    useStateでstartとendの設定

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, { Dispatch, SetStateAction } from 'react'
 
 import styles from './index.module.css'
 
-export const StartEndForm: React.FC = () => {
+type Props = {
+  setStart: Dispatch<SetStateAction<number | undefined>>
+  setEnd: Dispatch<SetStateAction<number | undefined>>
+}
+
+export const StartEndForm: React.FC<Props> = ({ setStart, setEnd }) => {
   return (
     <form className={styles.form}>
       <label>
         開始：
-        <input type="text" name="start" />
+        <input
+          type="text"
+          name="start"
+          onChange={(e) => setStart(parseInt(e.target.value))}
+        />
       </label>
       <label>
         終了：
-        <input type="text" name="end" />
+        <input
+          type="text"
+          name="end"
+          onChange={(e) => setEnd(parseInt(e.target.value))}
+        />
       </label>
       <input type="submit" value="Play!" />
     </form>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { NextPage } from 'next'
 import YouTube, { Options } from 'react-youtube'
 
@@ -5,16 +6,18 @@ import { StartEndForm } from '@/components/form'
 import { YplayerHeader } from '@/components/header'
 
 const IndexPage: NextPage = () => {
+  const [start, setStart] = React.useState<number | undefined>(undefined)
+  const [end, setEnd] = React.useState<number | undefined>(undefined)
   const opts: Options = {
     height: '390',
     width: '640',
-    playerVars: { start: 5, end: 10 },
+    playerVars: { start, end },
   }
   return (
     <>
       <YplayerHeader />
       <YouTube videoId="2g811Eo7K8U" opts={opts} />
-      <StartEndForm />
+      <StartEndForm setStart={setStart} setEnd={setEnd} />
     </>
   )
 }
