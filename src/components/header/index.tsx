62b5254e10b0dc2068e74cea6a5bd2608bdfcc52
hiroki_iizuka <alljapan.ma.kabu@gmail.com>
2020-09-25 17:51:52 +0900

    デザイン

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -9,7 +9,7 @@ type Props = {
 export const YplayerHeader: React.FC<Props> = ({ setVideoId }) => {
   return (
     <header className={styles.header}>
-      <div className="{styles.left}">Y-player</div>
+      <div className={styles.left}>Y-player</div>
       <div className={styles.center}>
         <input
           className={styles.searchInput}
